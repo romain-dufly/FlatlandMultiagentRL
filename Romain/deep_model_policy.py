8c82c37e627bbd233b827f573f54499c500810e4
romain-dufly <romain.dufly@stjoavignon.com>
2024-03-06 16:17:17 +0100

    Consolidation Romain

---
+++ Romain/deep_model_policy.py
@@ -0,0 +1,299 @@
+import copy
+import os
+import pickle
+import random
+from collections import namedtuple, deque, Iterable
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+class PolicyParameters:
+    def __init__(self, action_size, buffer_size, batch_size, update_every, gamma, tau, lr, use_gpu):
+        self.action_size = action_size
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.update_every = update_every
+        self.gamma = gamma
+        self.tau = tau
+        self.learning_rate = lr
+        self.use_gpu = use_gpu
+
+class Policy:
+    def step(self, state, action, reward, next_state, done):
+        raise NotImplementedError
+
+    def act(self, state, eps=0.):
+        raise NotImplementedError
+
+    def save(self, filename):
+        raise NotImplementedError
+
+    def load(self, filename):
+        raise NotImplementedError
+
+class DeepPolicy(Policy):
+    """
+    Wrapper on a network that defines a policy for the agent
+    Inputs: model (an extension of nn.Module)
+            p (parameters in the form of a class)
+            evaluation_mode (boolean)
+    """
+    def __init__(self, model, p, evaluation_mode=False):
+        
+        self.evaluation_mode = evaluation_mode
+        self.model = model
+        self.action_size = p.action_size
+
+        if p.use_gpu and torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+
+        if not evaluation_mode:
+            self.target = copy.deepcopy(self.model) # target network
+            self.optimizer = optim.Adam(self.model.parameters(), lr=p.learning_rate)
+            self.memory = ReplayBuffer(p.action_size, p.buffer_size, p.batch_size, self.device)
+            self.batch_size = p.batch_size
+            self.gamma = p.gamma # discount factor
+            self.tau = p.tau # for soft update of target parameters
+            self.update_every = p.update_every # interval for updating the network
+            self.t_step = 0 # counter for learning steps
+
+    def act(self, state, eps=0.):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+        self.model.eval()
+        with torch.no_grad():
+            action_values = self.model(state)
+        self.model.train()
+
+        # Use an epsilon-greedy policy
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def step(self, state, action, reward, next_state, done):
+        assert not self.evaluation_mode, "Policy set to evaluation only."
+
+        # Add to memory
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn at regular intervals
+        self.t_step = (self.t_step + 1) % self.update_every
+        if self.t_step == 0:
+            if len(self.memory) > self.batch_size:
+                experiences = self.memory.sample()
+                self.learn(experiences)
+
+    def learn(self, experiences):
+        """Update model parameters using given batch of experience."""
+        states, actions, rewards, next_states, dones = experiences
+
+        # Get expected Q values from local model
+        Q_expected = self.model(states).gather(1, actions)
+
+        # Compute Q targets for current states
+        Q_best_action = self.model(next_states).max(1)[1]
+        Q_targets_next = self.target(next_states).gather(1, Q_best_action.unsqueeze(-1))
+        Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Update target network
+        for target_param, model_param in zip(self.target.parameters(), self.model.parameters()):
+            target_param.data.copy_(self.tau * model_param.data + (1.0 - self.tau) * target_param.data)
+        
+    def save(self, filename):
+        torch.save(self.model.state_dict(), filename)
+
+    def load(self, filename):
+        self.model.load_state_dict(torch.load(filename))
+        self.target = copy.deepcopy(self.model)
+    
+    def save_replay_buffer(self, filename, size):
+        memory = self.memory.memory
+        with open(filename, 'wb') as f:
+            pickle.dump(list(memory)[-size:], f)
+    
+    def load_replay_buffer(self, filename):
+        with open(filename, 'rb') as f:
+            self.memory.memory = pickle.load(f)
+
+Experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+class ReplayBuffer:
+    def __init__(self, action_size, buffer_size, batch_size, device):
+        """Initialize a ReplayBuffer object.
+
+        Inputs: action_size (int): dimension of each action
+                buffer_size (int): maximum size of buffer
+                batch_size (int): size of each training batch
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size) # double ended queue
+        self.batch_size = batch_size
+        self.device = device
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = Experience(np.expand_dims(state, 0), action, reward, np.expand_dims(next_state, 0), done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(self.__v_stack_impr([e.state for e in experiences if e is not None])) \
+            .float().to(self.device)
+        actions = torch.from_numpy(self.__v_stack_impr([e.action for e in experiences if e is not None])) \
+            .long().to(self.device)
+        rewards = torch.from_numpy(self.__v_stack_impr([e.reward for e in experiences if e is not None])) \
+            .float().to(self.device)
+        next_states = torch.from_numpy(self.__v_stack_impr([e.next_state for e in experiences if e is not None])) \
+            .float().to(self.device)
+        dones = torch.from_numpy(self.__v_stack_impr([e.done for e in experiences if e is not None]).astype(np.uint8)) \
+            .float().to(self.device)
+
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):
+        return len(self.memory)
+
+    def __v_stack_impr(self, states):
+        sub_dim = len(states[0][0]) if isinstance(states[0], Iterable) else 1
+        np_states = np.reshape(np.array(states), (len(states), sub_dim))
+        return np_states
+
+'''
+class DDDQNPolicy(Policy):
+    """Dueling Double DQN policy"""
+
+    def __init__(self, state_size, action_size, parameters, evaluation_mode=False):
+        self.evaluation_mode = evaluation_mode
+
+        self.state_size = state_size
+        self.action_size = action_size
+        self.double_dqn = True
+        self.hidsize = 1
+
+        if not evaluation_mode:
+            self.hidsize = parameters.hidden_size
+            self.buffer_size = parameters.buffer_size
+            self.batch_size = parameters.batch_size
+            self.update_every = parameters.update_every
+            self.learning_rate = parameters.learning_rate
+            self.tau = parameters.tau
+            self.gamma = parameters.gamma
+            self.buffer_min_size = parameters.buffer_min_size
+
+        # Device
+        if parameters.use_gpu and torch.cuda.is_available():
+            self.device = torch.device("cuda:0")
+            # print("🐇 Using GPU")
+        else:
+            self.device = torch.device("cpu")
+            # print("🐢 Using CPU")
+
+        # Q-Network
+        self.qnetwork_local = DuelingQNetwork(state_size, action_size, hidsize1=self.hidsize, hidsize2=self.hidsize).to(self.device)
+
+        if not evaluation_mode:
+            self.qnetwork_target = copy.deepcopy(self.qnetwork_local)
+            self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.learning_rate)
+            self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size, self.device)
+
+            self.t_step = 0
+            self.loss = 0.0
+
+    def act(self, state, eps=0.):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def step(self, state, action, reward, next_state, done):
+        assert not self.evaluation_mode, "Policy has been initialized for evaluation only."
+
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % self.update_every
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > self.buffer_min_size and len(self.memory) > self.batch_size:
+                self._learn()
+
+    def _learn(self):
+        experiences = self.memory.sample()
+        states, actions, rewards, next_states, dones = experiences
+
+        # Get expected Q values from local model
+        q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        if self.double_dqn:
+            # Double DQN
+            q_best_action = self.qnetwork_local(next_states).max(1)[1]
+            q_targets_next = self.qnetwork_target(next_states).gather(1, q_best_action.unsqueeze(-1))
+        else:
+            # DQN
+            q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(-1)
+
+        # Compute Q targets for current states
+        q_targets = rewards + (self.gamma * q_targets_next * (1 - dones))
+
+        # Compute loss
+        self.loss = F.mse_loss(q_expected, q_targets)
+
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        self.loss.backward()
+        self.optimizer.step()
+
+        # Update target network
+        self._soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
+
+    def _soft_update(self, local_model, target_model, tau):
+        # Soft update model parameters.
+        # θ_target = τ*θ_local + (1 - τ)*θ_target
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+    def save(self, filename):
+        torch.save(self.qnetwork_local.state_dict(), filename + ".local")
+        torch.save(self.qnetwork_target.state_dict(), filename + ".target")
+
+    def load(self, filename):
+        if os.path.exists(filename + ".local"):
+            self.qnetwork_local.load_state_dict(torch.load(filename + ".local"))
+        if os.path.exists(filename + ".target"):
+            self.qnetwork_target.load_state_dict(torch.load(filename + ".target"))
+
+    def save_replay_buffer(self, filename):
+        memory = self.memory.memory
+        with open(filename, 'wb') as f:
+            pickle.dump(list(memory)[-500000:], f)
+
+    def load_replay_buffer(self, filename):
+        with open(filename, 'rb') as f:
+            self.memory.memory = pickle.load(f)
+
+    def test(self):
+        self.act(np.array([[0] * self.state_size]))
+        self._learn()
+'''
---
+++ Romain/models.py
@@ -0,0 +1,289 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from impl_config import FeatureParserConfig as fp
+from impl_config import NetworkConfig as ns
+
+class DuelingQNetwork(nn.Module):
+    """Dueling Q-network (https://arxiv.org/abs/1511.06581)"""
+
+    def __init__(self, state_size, action_size, hidsize1=128, hidsize2=128):
+        super(DuelingQNetwork, self).__init__()
+
+        # value network
+        self.fc1_val = nn.Linear(state_size, hidsize1)
+        self.fc2_val = nn.Linear(hidsize1, hidsize2)
+        self.fc4_val = nn.Linear(hidsize2, 1)
+
+        # advantage network
+        self.fc1_adv = nn.Linear(state_size, hidsize1)
+        self.fc2_adv = nn.Linear(hidsize1, hidsize2)
+        self.fc4_adv = nn.Linear(hidsize2, action_size)
+
+    def forward(self, x):
+        val = F.relu(self.fc1_val(x))
+        val = F.relu(self.fc2_val(val))
+        val = self.fc4_val(val)
+
+        # advantage calculation
+        adv = F.relu(self.fc1_adv(x))
+        adv = F.relu(self.fc2_adv(adv))
+        adv = self.fc4_adv(adv)
+
+        return val + adv - adv.mean()
+    
+class TreeLSTM(nn.Module):
+    """
+    https://github.com/unbounce/pytorch-tree-lstm/blob/master/treelstm/tree_lstm.py#L10
+    """
+
+    def __init__(self, in_features, out_features) -> None:
+        """TreeLSTM class initializer
+        Takes in int sizes of in_features and out_features and sets up model Linear network layers.
+        """
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        # bias terms are only on the W layers for efficiency
+        self.W_iou = torch.nn.Linear(self.in_features, 3 * self.out_features)
+        self.U_iou = torch.nn.Linear(
+            3 * self.out_features, 3 * self.out_features, bias=False
+        )
+        # self.W_h = torch.nn.Linear(3 * self.out_features, self.out_features)
+        self.W_c = torch.nn.Linear(3 * self.out_features, self.out_features)
+
+        # f terms are maintained seperate from the iou terms because they involve sums over child nodes
+        # while the iou terms do not
+        self.W_f = torch.nn.Linear(self.in_features, self.out_features)
+        self.U_f = torch.nn.Linear(self.out_features, self.out_features, bias=False)
+
+    def forward(self, forest, adjacency, node_order, edge_order):
+        """Run TreeLSTM model on a tree data structure with node features
+        Takes Tensors encoding node features, a tree node adjacency_list, and the order in which
+        the tree processing should proceed in node_order and edge_order.
+        """
+        forest = forest.flatten(0, 2)
+        adjacency_list = adjacency.flatten(0, 2)
+        node_order = node_order.flatten(0, 2)
+        edge_order = edge_order.flatten(0, 2)
+
+        # Total number of nodes in every tree in the batch
+        batch_size = node_order.shape[0]
+
+        # Retrive device the model is currently loaded on to generate h, c, and h_sum result buffers
+        device = next(self.parameters()).device
+
+        # h and c states for every node in the batch
+        h = torch.zeros(batch_size, self.out_features, device=device)
+        c = torch.zeros(batch_size, self.out_features, device=device)
+
+        # populate the h and c states respecting computation order
+        for n in range(node_order.max() + 1):
+            self._run_lstm(n, h, c, forest, node_order, adjacency_list, edge_order)
+        return h
+
+    # @torchsnooper.snoop()
+    def _run_lstm(
+        self,
+        iteration: int,
+        h: torch.Tensor,
+        c: torch.Tensor,
+        features: torch.Tensor,
+        node_order: torch.Tensor,
+        adjacency_list: torch.Tensor,
+        edge_order: torch.Tensor,
+    ):
+        """Helper function to evaluate all tree nodes currently able to be evaluated."""
+        # N is the number of nodes in the tree
+        # n is the number of nodes to be evaluated on in the current iteration
+        # E is the number of edges in the tree
+        # e is the number of edges to be evaluated on in the current iteration
+        # F is the number of features in each node
+        # M is the number of hidden neurons in the network
+
+        # node_order is a tensor of size N x 1
+        # edge_order is a tensor of size E x 1
+        # features is a tensor of size N x F
+        # adjacency_list is a tensor of size E x 2
+
+        # node_mask is a tensor of size N x 1
+        node_mask = node_order == iteration
+        # edge_mask is a tensor of size E x 1
+        edge_mask = edge_order == iteration
+
+        # x is a tensor of size n x F
+        x = features[node_mask, :]
+
+        # At iteration 0 none of the nodes should have children
+        # Otherwise, select the child nodes needed for current iteration
+        # and sum over their hidden states
+        if iteration == 0:
+            iou = self.W_iou(x)
+        else:
+            # adjacency_list is a tensor of size e x 2
+            adjacency_list = adjacency_list[edge_mask, :]
+
+            # parent_indexes and child_indexes are tensors of size e x 1
+            # parent_indexes and child_indexes contain the integer indexes needed to index into
+            # the feature and hidden state arrays to retrieve the data for those parent/child nodes.
+            parent_indexes = adjacency_list[:, 0]
+            child_indexes = adjacency_list[:, 1]
+
+            # child_h and child_c are tensors of size e x 1
+            child_h = h[child_indexes, :]
+            child_c = c[child_indexes, :]
+
+            # # Add child hidden states to parent offset locations
+            # _, child_counts = torch.unique_consecutive(parent_indexes, return_counts=True)
+            # child_counts = tuple(child_counts)
+
+            # parent_children = torch.split(child_h, child_counts)
+            # parent_list = [item.sum(0) for item in parent_children]
+
+            # h_sum = torch.stack(parent_list)
+
+            i_dims = child_h.shape[0] // 3
+            child_h_merge = child_h.unflatten(0, (i_dims, 3)).flatten(start_dim=1)
+            # h_reduce = self.W_h(child_h_merge)
+
+            iou = self.W_iou(x) + self.U_iou(child_h_merge)
+
+        # i, o and u are tensors of size n x M
+        i, o, u = torch.split(iou, iou.size(1) // 3, dim=1)
+        i = torch.sigmoid(i)
+        o = torch.sigmoid(o)
+        u = torch.tanh(u)
+
+        # At iteration 0 none of the nodes should have children
+        # Otherwise, calculate the forget states for each parent node and child node
+        # and sum over the child memory cell states
+        if iteration == 0:
+            c[node_mask, :] = i * u
+        else:
+            # f is a tensor of size e x M
+            f = self.W_f(features[parent_indexes, :]) + self.U_f(child_h)
+            f = torch.sigmoid(f)
+
+            # fc is a tensor of size e x M
+            fc = f * child_c
+
+            # Add the calculated f values to the parent's memory cell state
+            # parent_children = torch.split(fc, child_counts)
+            # parent_list = [item.sum(0) for item in parent_children]
+
+            # c_sum = torch.stack(parent_list)
+
+            fc = fc.unflatten(0, (fc.shape[0] // 3, 3)).flatten(start_dim=1)
+            c_reduce = self.W_c(fc)
+
+            c[node_mask, :] = i * u + c_reduce
+
+        h[node_mask, :] = o * torch.tanh(c[node_mask])
+
+
+class Transformer(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super(Transformer, self).__init__()
+        self.attention = nn.MultiheadAttention(embed_dim, num_heads)
+        self.att_mlp = nn.Sequential(
+            nn.Linear(embed_dim * 2, embed_dim),
+            nn.GELU(),
+        )
+
+    def forward(self, input):
+        batch_size, n_agents, embedding_size = input.shape
+        input = input.permute(1, 0, 2)  # agents, batch, embedding
+        input = input.view(n_agents, -1, embedding_size)
+        output, _ = self.attention(input, input, input)
+
+        input = input.view(n_agents, -1, embedding_size)
+        input = input.permute(1, 0, 2)
+        output = output.view(n_agents, -1, embedding_size)
+        output = output.permute(1, 0, 2)
+        output = self.att_mlp(torch.cat([input, output], dim=-1))
+        return output
+
+class TransLSTM(nn.Module):
+    """
+    Feature:  cat(agents_attr_embedding, tree_embedding)
+    structure: mlp
+    """
+    def __init__(self):
+        super(TransLSTM, self).__init__()
+        self.tree_lstm = TreeLSTM(fp.node_sz, ns.tree_embedding_sz)
+        self.attr_embedding = nn.Sequential(
+            nn.Linear(fp.agent_attr, 2 * ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(2 * ns.hidden_sz, 2 * ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(2 * ns.hidden_sz, 2 * ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(2 * ns.hidden_sz, ns.hidden_sz),
+            nn.GELU(),
+        )
+        self.transformer = nn.Sequential(
+            Transformer(ns.hidden_sz + ns.tree_embedding_sz, 4),
+            Transformer(ns.hidden_sz + ns.tree_embedding_sz, 4),
+            Transformer(ns.hidden_sz + ns.tree_embedding_sz, 4),
+        )
+        self.actor_net = nn.Sequential(
+            nn.Linear(ns.hidden_sz * 2 + ns.tree_embedding_sz * 2, 2 * ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(ns.hidden_sz * 2, ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(ns.hidden_sz, fp.action_sz),
+        )
+        self.critic_net = nn.Sequential(
+            nn.Linear(ns.hidden_sz * 2 + ns.tree_embedding_sz * 2, ns.hidden_sz * 2),
+            nn.GELU(),
+            nn.Linear(ns.hidden_sz * 2, ns.hidden_sz),
+            nn.GELU(),
+            nn.Linear(ns.hidden_sz, 1),
+        )
+
+    def forward(self, agents_attr, forest, adjacency, node_order, edge_order):
+        batch_size, n_agents, num_nodes, _ = forest.shape
+        device = next(self.parameters()).device
+        adjacency = self.modify_adjacency(adjacency, device)
+
+        tree_embedding = self.tree_lstm(forest, adjacency, node_order, edge_order)
+        tree_embedding = tree_embedding.unflatten(0, (batch_size, n_agents, num_nodes))[
+            :, :, 0, :
+        ]
+
+        agent_attr_embedding = self.attr_embedding(agents_attr)
+        embedding = torch.cat([agent_attr_embedding, tree_embedding], dim=2)
+
+        ## attention
+        att_embedding = self.transformer(embedding)
+
+        worker_action = torch.zeros((batch_size, n_agents, 5), device=device)
+        worker_action[:, :n_agents, :] = self.actor(embedding, att_embedding)
+        critic_value = self.critic(embedding, att_embedding)
+        return [worker_action], critic_value  # (batch size, 1)
+
+    def actor(self, embedding, att_embedding):
+        worker_action = torch.cat([embedding, att_embedding], dim=-1)
+        worker_action = self.actor_net(worker_action)
+        return worker_action
+
+    def critic(self, embedding, att_embedding):
+        output = torch.cat([embedding, att_embedding], dim=-1)
+        critic_value = self.critic_net(output)
+        critic_value = critic_value.mean(1).view(-1)
+        return critic_value
+
+    def modify_adjacency(self, adjacency, _device):
+        batch_size, n_agents, num_edges, _ = adjacency.shape
+        num_nodes = num_edges + 1
+        id_tree = torch.arange(0, batch_size * n_agents, device=_device)
+        id_nodes = id_tree.view(batch_size, n_agents, 1)
+        adjacency[adjacency == -2] = (
+            -batch_size * n_agents * num_nodes
+        )  # node_idx == -2 invalid node
+        adjacency[..., 0] += id_nodes * num_nodes
+        adjacency[..., 1] += id_nodes * num_nodes
+        adjacency[adjacency < 0] = -2
+        return adjacency
